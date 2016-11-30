bd078e00b203dd651609f68c93963d237872bad5
tabish.khan <tabish.khan@tothenew.com>
2016-11-30 17:15:54 +0530

    Added the loader for wait time to fetch the data

--- public/js/controllers/TestChatCtrl.js
+++ public/js/controllers/TestChatCtrl.js
@@ -1,12 +1,12 @@
 angular.module('sampleApp').controller('TestChatController', function($scope, $http) {
 
   $scope.innterHeight = window.innerHeight / 3;
-
+  var channelssss;
   var sb = new SendBird({
     appId: "D5B18F28-22F1-4D72-ABFA-F66C0BAAE04C"
   });
 
-  sb.connect("TABISHSSSS", function(user, error) {
+  sb.connect("KMT", function(user, error) {
     console.log("USER:", user)
     var openChannelListQuery = sb.OpenChannel.createOpenChannelListQuery();
 
@@ -23,32 +23,62 @@ angular.module('sampleApp').controller('TestChatController', function($scope, $h
           console.error(error);
           return;
         }
-
-        channel.enter(function(response, error){
+        channelssss = channel;
+        channelssss.enter(function(response, error){
           if (error) {
             console.error(error);
             return;
           }else
           console.log("Channel Join Response:", response)
-          channel.sendUserMessage("Hello KHAN 1How are youwwwwwww?", "", function(message, error){
-            if (error) {
-              console.error(error);
-              return;
-            }
-            console.log(message);
-            var ChannelHandler = new sb.ChannelHandler();
 
-            ChannelHandler.onMessageReceived = function(channel, message){
-              console.log(channel, message);
-            };
-            sb.addChannelHandler("myChannel", ChannelHandler);
-          });
+        });
+
+        var messageListQuery = channelssss.createPreviousMessageListQuery();
+
+        messageListQuery.load(20, true, function(messageList, error){
+          if (error) {
+            console.error(error);
+            return;
+          }
+          $scope.messageList = messageList;
+          $scope.$digest();
+          console.log(messageList);
         });
       });
     });
   });
 
+$scope.send = function(){
+  console.log("lllllllll",$scope.message)
+  channelssss.sendUserMessage($scope.message, "", function(message, error){
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(message);
+    $scope.messageList.push(message);
+    $scope.$digest();
+
+  });
+}
+
+  var ChannelHandler = new sb.ChannelHandler();
+
+  ChannelHandler.onMessageReceived = function(channel, message){
+    console.log(channel, message);
+    $scope.messageList.push(message);
+    $scope.$digest();
 
 
+  };
+  sb.addChannelHandler("myChannel", ChannelHandler);
 
 })
+
+
+/*We are implementing the following flow:
+* The user automaticaly created with their email and name when they want to access the chat
+ * If user is already created then we will just use that user id to send the message
+ * Whenever any new event is created we will also create a chat group with the same name
+ * Registered user of that event can group chat for the same event
+ * */
